821c1f11bd36bb576d2c9c39305473ed8ba1bc15
cristian <Heavyblade@users.noreply.github.com>
2025-09-07 21:17:21 -0500

    Adding improvements

--- app/controllers/admin/time_off_requests_controller.rb
+++ app/controllers/admin/time_off_requests_controller.rb
@@ -4,8 +4,9 @@ module Admin
     before_action :require_admin!
 
     def index
-      @time_off_requests = TimeOffRequest.order(created_at: :desc)
+      scope = TimeOffRequest.order(created_at: :desc)
+      scope = scope.where(status: :pending) unless params[:all] == '1'
+      @time_off_requests = scope
     end
   end
 end
-
