bbae9a1e067600783bf967bf699f19fe9f36937a
cristian <Heavyblade@users.noreply.github.com>
2025-09-08 07:54:35 -0500

    Updating configs

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}',
+    './vendor/javascript/**/*.js',
+    './node_modules/flowbite/**/*.js'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('flowbite/plugin')
+  ],
+}
