b44ba74d77257a9b138b0b5fff616381ed3b0456
cristian <Heavyblade@users.noreply.github.com>
2025-09-08 06:42:55 -0500

    Ensuring flowbite componenets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,12 @@ module ApplicationHelper
       'bg-gray-100 border-gray-500 text-gray-700'
     end
   end
+
+  def class_for_flash(key)
+      case key.to_s
+      when 'notice' then 'flex items-center p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400'
+      when 'alert' then 'flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400'
+      else 'flex items-center p-4 mb-4 text-sm text-gray-800 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-300'
+      end
+  end
 end
